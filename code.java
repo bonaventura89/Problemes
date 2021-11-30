1403813410e2bd06d69bfe009c2b78ea9a9cddda
bonaventura89 <57214868+bonaventura89@users.noreply.github.com>
2021-11-30 11:07:41 +0100

    Update code.java (#2)
    Canvis en el codi.java

--- code.java
+++ code.java
@@ -2,20 +2,29 @@ public class Belly {
   private int cukes;
   
   public Belly() {
+    if(cukes != 0)
+    {
       this.cukes = 0;
+    }
   }
   
   public void eat(int cukes) {
-      this.cukes += cukes;
+    if(cukes >= 0)
+    {
+       this.cukes += cukes;
+    }
   }
   
   public void digest(int hour) {
+    if(cukes > 0)
+    {
       this.cukes -= hour;
+    }
   }
   
   public int getCukes() {
-      return cukes;
-   
+      return cukes; 
   }
+  
   
 }
